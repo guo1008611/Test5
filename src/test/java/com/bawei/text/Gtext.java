23590aa07b807d729343455fb8a3cb4ed9198839
Your Name <1148200593@qq.com>
2019-06-13 08:30:02 +0800

    日考第五天提交项目到git

---
+++ src/test/java/com/bawei/text/Gtext.java
@@ -0,0 +1,13 @@
+package com.bawei.text;
+
+
+
+public class Gtext {
+	public static void main(String[] args) {
+		/*（一）使用Maven创建Java项目。（10分）
+		（二）编写main方法，输出helloWord。（10分）*/
+		System.out.println("hello word");
+	/*	（四）提交项目代码到Git仓库（10分）
+		修改项目代码，再次提交Git仓库。（*/
+	}
+}
