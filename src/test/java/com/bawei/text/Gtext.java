54db00c5eec72fd9a83b89c888a99a07daee68b8
Your Name <1148200593@qq.com>
2019-06-13 08:32:06 +0800

    修改后提交

--- src/test/java/com/bawei/text/Gtext.java
+++ src/test/java/com/bawei/text/Gtext.java
@@ -6,7 +6,7 @@ public class Gtext {
 	public static void main(String[] args) {
 		/*（一）使用Maven创建Java项目。（10分）
 		（二）编写main方法，输出helloWord。（10分）*/
-		System.out.println("hello word");
+		System.out.println("hello word   修改 后提交");
 	/*	（四）提交项目代码到Git仓库（10分）
 		修改项目代码，再次提交Git仓库。（*/
 	}
